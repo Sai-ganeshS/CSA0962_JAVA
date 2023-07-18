95203219e257f00383f8bd7ddda78ec4903d5cc2
Sai-ganeshS <112494146+Sai-ganeshS@users.noreply.github.com>
2023-07-18 20:23:32 +0530

    output
    Enter the radius:4
    area of circle : 50.24
    Circumfrences of circle : 25.12

---
+++ area_of_circle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class j{
+public static void main(String[] args)
+{
+	int a,b;
+	Scanner scan=new Scanner(System.in);
+	System.out.print("Enter the radius:");
+	a=scan.nextInt();
+	
+
+
+	System.out.println("area of circle : "+(3.14*a*a));
+	System.out.println("Circumfrences of circle : "+(3.14*2*a));
+
+
+}
+
+
+
+}
