fb574e762e241646ff1473608bd83fbe3e0fa167
Sai-ganeshS <112494146+Sai-ganeshS@users.noreply.github.com>
2023-07-18 20:14:41 +0530

    Create sum_of_5,avg.java
    enter the 1st no: 10
    enter the 2nd no: 20
    enter the 3rd no: 30
    enter the 4th no: 10
    enter the 5th no: 15
    
    sum of 5 numbers: 85
    average : 17

---
+++ sum_of_5,avg.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class j
+{
+	public static void main(String args[])
+	{
+		Scanner scan=new Scanner(System.in);
+		int a,b,c,d,e;
+		
+		System.out.print("enter the 1st no: ");
+		a=scan.nextInt();
+		System.out.print("enter the 2nd no: ");
+		b=scan.nextInt();
+		System.out.print("enter the 3rd no: ");
+		c=scan.nextInt();
+		System.out.print("enter the 4th no: ");
+		d=scan.nextInt();
+		System.out.print("enter the 5th no: ");
+		e=scan.nextInt();
+		
+		
+
+		System.out.println();
+		System.out.println("sum of 5 numbers: "+(a+b+c+d+e) );
+		System.out.println("average : "+(a+b+c+d+e)/5 );
+		
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
