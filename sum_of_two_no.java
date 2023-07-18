291666087f217d3922fc6b38658274ff9eaacbb4
Sai-ganeshS <112494146+Sai-ganeshS@users.noreply.github.com>
2023-07-18 20:20:58 +0530

    output
    Enter the number:10
    Enter the number:9
    Sum of two numbers : 19

--- sum_of_two_no.java
+++ sum_of_two_no.java
@@ -5,7 +5,7 @@ public static void main(String[] args)
 {
 	int a,b;
 	Scanner scan=new Scanner(System.in);
-	System.out.print("Enter the number:");
+	System.out.print("Enter the number:") ;
 	a=scan.nextInt();
 	System.out.print("Enter the number:");
 	b=scan.nextInt();
