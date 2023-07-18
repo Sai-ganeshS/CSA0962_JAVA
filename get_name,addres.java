cf451363582476b2f3806524a5779c3e80546382
Sai-ganeshS <112494146+Sai-ganeshS@users.noreply.github.com>
2023-07-18 20:30:09 +0530

    output
    Enter the name:sai ganesh
    Enter the Father name:g sttu
    Enter the door no:5/68
    Enter the street name:mmda
    Enter the area:maduravoyal
    Enter the city:chennai
    --------------Address---------
    name: sai ganesh
    Father name:  g sttu
    door no: 5/68
    street name:  mmda
     area:  maduravoyal
    city:  chennai

---
+++ get_name,addres.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class j{
+public static void main(String[] args)
+{
+	String a,b,c,d,e,f;
+	Scanner scan=new Scanner(System.in);
+	System.out.print("Enter the name:");
+	a=scan.nextLine();
+
+	System.out.print("Enter the Father name:");
+	b=scan.nextLine();
+
+	System.out.print("Enter the door no:");
+	c=scan.nextLine();
+
+	System.out.print("Enter the street name:");
+	d=scan.nextLine();
+
+	System.out.print("Enter the area:");
+	e=scan.nextLine();
+
+	System.out.print("Enter the city:");
+	f=scan.nextLine();
+
+
+	System.out.println("--------------Address---------");
+	
+	System.out.println("name: "+a);
+
+
+	System.out.println("Father name:  "+b);
+	
+
+	System.out.println("door no: "+c);
+	
+
+	System.out.println("street name:  "+d);
+
+
+	System.out.println(" area:  "+e);
+	
+
+	System.out.println("city:  "+f);
+	
+
+	
+	
+
+
+
+
+
+}
+
+
+
+}
