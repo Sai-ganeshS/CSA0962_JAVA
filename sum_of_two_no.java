0ffcb68dd03a2b255bff5d2438e7bbe0171fe9ea
Sai-ganeshS <112494146+Sai-ganeshS@users.noreply.github.com>
2023-07-18 20:20:28 +0530

    Create sum_of_two_no.java
    Enter the number:10
    Enter the number:9
    Sum of two numbers : 19

---
+++ sum_of_two_no.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class j{
+public static void main(String[] args)
+{
+	int a,b;
+	Scanner scan=new Scanner(System.in);
+	System.out.print("Enter the number:");
+	a=scan.nextInt();
+	System.out.print("Enter the number:");
+	b=scan.nextInt();
+
+
+	System.out.println("Sum of two numbers : "+(a+b));
+	
+
+
+}
+
+
+
+}
