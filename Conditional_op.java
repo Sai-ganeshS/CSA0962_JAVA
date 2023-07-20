332305fc02f4d9ab68b3d0d5a2bd83a3b1266b9b
Sai-ganeshS <112494146+Sai-ganeshS@users.noreply.github.com>
2023-07-20 07:45:14 +0530

    Create Conditional_op.java

---
+++ Conditional_op.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class j
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int a,b;
+System.out.println("Enter the a: ");
+a=scan.nextInt();
+System.out.println("Enter the b: ");
+b=scan.nextInt();
+
+
+System.out.println((a<b));
+System.out.println((a>b));
+System.out.println( (a==b));
+System.out.println((a<=b));
+System.out.println((a>=b));
+
+
+
+
+}
+
+}
