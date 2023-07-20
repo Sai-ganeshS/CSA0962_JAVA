3772ad913522f272c54744e0f92bd3878b06ec12
Sai-ganeshS <112494146+Sai-ganeshS@users.noreply.github.com>
2023-07-20 07:50:06 +0530

    Create greatest_of_three.java

---
+++ greatest_of_three.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class j
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int x,y,z;
+System.out.println("");
+System.out.println("------------input-----------");
+System.out.println("Enter the X value: ");
+x=scan.nextInt();
+System.out.println("Enter the Y value: ");
+y=scan.nextInt();
+System.out.println("Enter the Z value: ");
+z=scan.nextInt();
+
+
+System.out.println("------------output-----------");
+if(x>y && x>z)
+{
+System.out.println("X value is greater");
+}
+else if(y>z &&y>x)
+System.out.println("y value is greater");
+else
+{
+	System.out.println("z value is greater");
+	
+}
+
+
+
+
+
+System.out.println("               ");
+System.out.println("");
+System.out.println("");
+System.out.println("");
+
+
+}}
