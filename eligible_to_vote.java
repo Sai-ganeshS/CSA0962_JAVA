db0c4bffb0155edead3d0627d350254d8a5d0ce4
Sai-ganeshS <112494146+Sai-ganeshS@users.noreply.github.com>
2023-07-20 07:47:19 +0530

    Create eligible_to_vote.java

---
+++ eligible_to_vote.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class j
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int x;
+System.out.println("");
+System.out.println("------------input-----------");
+x=scan.nextInt();
+
+
+
+System.out.println("------------output-----------");
+if(x>=18)
+{
+System.out.println("Eligible to Vote");
+}
+else
+{
+	System.out.println("not elibigle to vote");
+	System.out.println("More "+(18-x)+" years left to vote");
+}
+System.out.println("               ");
+System.out.println("");
+System.out.println("");
+System.out.println("");
+
+
+
+
+
+}
+
+}
