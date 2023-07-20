21f9b8b1bc71866f818fe169f6aea21d1c362da7
Sai-ganeshS <112494146+Sai-ganeshS@users.noreply.github.com>
2023-07-20 07:47:50 +0530

    Create leap_year.java

---
+++ leap_year.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+class j
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int x;
+System.out.println("");
+System.out.println("------------input-----------");
+x=scan.nextInt();
+
+
+
+System.out.println("------------output-----------");
+if(x%4==0)
+{
+System.out.println("leap year");
+}
+else
+{
+	System.out.println("not a leap year");
+	System.out.println(" "+((4-(x%4))+x)+" ");
+}
+
+
+
+
+
+System.out.println("               ");
+System.out.println("");
+System.out.println("");
+System.out.println("");
+
+
+}}
