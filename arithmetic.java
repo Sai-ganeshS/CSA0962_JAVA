6bb128a8e6b392540e17d3abd1f75be9205fd22b
Sai-ganeshS <112494146+Sai-ganeshS@users.noreply.github.com>
2023-07-20 07:46:34 +0530

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class j
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int a,b;
+System.out.println("Enter the a: ");
+a=scan.nextInt();
+System.out.println("Enter the b: ");
+b=scan.nextInt();
+
+
+System.out.println( "Sum: "+(a+b));
+System.out.println( "Diff: : "+(a-b));
+System.out.println("MUL: "+ (a*b));
+System.out.println("Div: "+(a/b));
+System.out.println("Div: "+(a/b));
+
+}
+
+}
+
