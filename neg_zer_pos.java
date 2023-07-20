ac5bb00f98209f717d70d57a2e1db94c9ae175e0
Sai-ganeshS <112494146+Sai-ganeshS@users.noreply.github.com>
2023-07-20 07:49:27 +0530

    Create neg_zer_pos.java

---
+++ neg_zer_pos.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class j
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int x;
+System.out.println("");
+System.out.println("------------input-----------");
+x=scan.nextInt();
+
+
+
+System.out.println("------------output-----------");
+if(x<0)
+{
+System.out.println("Negative");
+}
+else if(x==0)
+System.out.println("Zero");
+else
+{
+	System.out.println("Postive");
+	
+}
+
+
+
+
+
+System.out.println("               ");
+System.out.println("");
+System.out.println("");
+System.out.println("");
+
+
+}}
